65bc74e1ee272dc49d93e3a2faa56b1e5e1e5fe7
Takako Onozuka <onozukatakako@gmail.com>
2022-10-26 22:16:25 +0100

    commit

--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -1,3 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import ChartList from '../components/ChartList.js';
 
+function ChartContainer() {
+
+    const [songs, setSongs] = useState([]);
+
+    async function fetchSongs() {
+        const response = await fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json');
+        const data = await response.json();
+        setSongs(data);
+    }
+    
+    useEffect(() => {
+        fetchSongs();
+    }, []);
+
+
+return (
+    <>
+        <h1>Music Chart</h1>
+        
+    </>
+);
+}
+export default ChartContainer;
